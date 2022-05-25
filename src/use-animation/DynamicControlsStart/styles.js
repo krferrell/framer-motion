11244d7dfdb27d32606337b504d9cb73ab97c2b5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-25 14:33:36 -0700

    feat(dynamic-controls-start): add flex display with center align-items for 'Container', and add auto to margin-right and margin-left for 'List'

--- src/use-animation/DynamicControlsStart/styles.js
+++ src/use-animation/DynamicControlsStart/styles.js
@@ -2,11 +2,16 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const Container = styled.section`
+  display: flex;
+  align-items: center;
   min-width: 100vw;
   min-height: 100vh;
 `;
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  margin-right: auto;
+  margin-left: auto;
+`;
 
 export const ListItem = styled(motion.li)`
   list-style: none;
