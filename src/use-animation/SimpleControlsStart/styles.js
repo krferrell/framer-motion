de53189d05185829f47fe01a7100505a09bd479d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-25 14:33:16 -0700

    feat(simple-controls-start): add flex display with center align-items for 'Container', and add auto to margin-right and margin-left for 'ImageWrapper'

--- src/use-animation/SimpleControlsStart/styles.js
+++ src/use-animation/SimpleControlsStart/styles.js
@@ -2,12 +2,16 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const Container = styled.section`
+  display: flex;
+  align-items: center;
   min-width: 100vw;
   min-height: 100vh;
 `;
 
 export const ImageWrapper = styled(motion.div)`
   width: 150px;
+  margin-right: auto;
+  margin-left: auto;
 `;
 
 export const Image = styled.img`
