fec146a5ef4c2f049c03d7361bd351f52417e6b2
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-05-30 16:33:32 -0700

    feat(codeblock): add a copiable code string

--- src/FramerDemo/FramerDemo.js
+++ src/FramerDemo/FramerDemo.js
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { MainContainer } from "./styles";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import AnimationGrid from "./Components/AnimationGrid/AnimationGrid";
 import CodeSnippet from "./Components/CodeSnippet/CodeSnippet";
 
 const FramerDemo = () => {
+
+    const [currAnimation, setCurrAnimation] = useState('');
+
     return (
         <MainContainer>
-            <Sidebar/>
+            <Sidebar setCurrAnimation={setCurrAnimation}/>
             <AnimationGrid/>
-            <CodeSnippet/>
+            <CodeSnippet currAnimation={currAnimation}/>
         </MainContainer>
     )
 }
